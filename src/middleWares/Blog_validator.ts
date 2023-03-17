e7b0ed8b993c7cc70c1a076efb6b908677925ef7
Andrew Marcker <ticey1995@gmail.com>
2023-03-17 13:46:52 +0200

    cmcmdclskm

--- src/middleWares/Blog_validator.ts
+++ src/middleWares/Blog_validator.ts
@@ -2,7 +2,15 @@ import {body} from "express-validator";
 
 export const checkName = body('name')
     .notEmpty()
+    .withMessage({
+        massage: "incorrect name",
+        field: "name"
+    })
     .isString()
+    .withMessage({
+        massage: "incorrect name",
+        field: "name"
+    })
     .isLength({ min: 1, max: 15 })
     .withMessage({
         massage: "incorrect name",
@@ -10,7 +18,15 @@ export const checkName = body('name')
     })
 export const checkDescription = body('description')
     .notEmpty()
+    .withMessage({
+        massage: "incorrect description",
+        field: "description"
+    })
     .isString()
+    .withMessage({
+        massage: "incorrect description",
+        field: "description"
+    })
     .isLength({ min: 1, max: 500 })
     .withMessage({
         massage: "incorrect description",
@@ -18,8 +34,20 @@ export const checkDescription = body('description')
     })
 export const checkUrl = body('websiteUrl')
     .notEmpty()
+    .withMessage({
+    massage: "incorrect websiteUrl",
+    field: "websiteUrl"
+})
     .isString()
+    .withMessage({
+        massage: "incorrect websiteUrl",
+        field: "websiteUrl"
+    })
     .isLength({ min: 1, max: 100 })
+    .withMessage({
+        massage: "incorrect websiteUrl",
+        field: "websiteUrl"
+    })
     .matches(/^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/)
     .withMessage({
         massage: "incorrect websiteUrl",
