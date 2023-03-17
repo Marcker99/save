0215dc5317a503c253291df031856e25e7a2ac50
Andrew Marcker <ticey1995@gmail.com>
2023-03-17 13:31:06 +0200

    back to previos v

--- src/middleWares/errors_Middleware.ts
+++ src/middleWares/errors_Middleware.ts
@@ -3,16 +3,14 @@ import {validationResult} from "express-validator";
 
 export const errorsMiddleware = (req:Request,res: Response, next:NextFunction)=>{
     const errors = validationResult(req)
-    const errorsMessages = []
+
     if(!errors.isEmpty()) {
         const errArr = errors.array()
-        for (let i = 0; i < errArr.length; i++) {
-            const error = errArr[i];
-            errorsMessages.push({
-                ...error.msg
-            });
-        }
-  ;
+        const resError = errArr.map((error) => ({
+            ...error.msg,
+        }));
+        //const resError = errArr[0].msg
+        const errorsMessages = [...resError]
 
         res.status(400).json({errorsMessages})
     } else {
